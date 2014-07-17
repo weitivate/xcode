50ec29e357a8e873fdd6edbfb4239c3183282840
dylan <weitivate@gmail.com>
2014-07-17 11:36:20 +0800

    added a test class

---
+++ Git Test/DWTest.h
@@ -0,0 +1,13 @@
+//
+//  DWTest.h
+//  Git Test
+//
+//  Created by Dylan Wei on 17/7/14.
+//  Copyright (c) 2014 weitivate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DWTest : NSObject
+
+@end
---
+++ Git Test/DWTest.m
@@ -0,0 +1,13 @@
+//
+//  DWTest.m
+//  Git Test
+//
+//  Created by Dylan Wei on 17/7/14.
+//  Copyright (c) 2014 weitivate. All rights reserved.
+//
+
+#import "DWTest.h"
+
+@implementation DWTest
+
+@end
